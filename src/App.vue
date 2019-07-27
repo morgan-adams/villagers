5772aee311b08ff7f476154f890d3a215a4687f8
Morgan <morgan.adams@lifelink.com>
2019-07-27 00:09:19 -0700

    Initial card design

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld msg="I wobo my bobo beawy muchos"/>
+    <VillagerCard name="Marshal"
+                  img="https://nookipedia.com/w/images/b/bb/Marshal_NL.png"
+                  species="squirrel"/>
+    <VillagerCard name="Lolly"
+                  img="https://nookipedia.com/w/images/0/05/Lolly_NLa.png"
+                  species="cat"/>
+    <VillagerCard name="Rosie"
+                  img="https://nookipedia.com/w/images/a/a1/Rosie_NLa.png"
+                  species="cat"/>
+    <VillagerCard name="Stitches"
+                  img="https://nookipedia.com/w/images/b/b1/Stitches_NLa.png"
+                  species="bear"/>
+    <VillagerCard name="Diana"
+                  img="https://nookipedia.com/w/images/c/c2/Diana_NLa.png"
+                  species="deer"/>
+    <VillagerCard name="Lucky"
+                  img="https://nookipedia.com/w/images/2/25/Lucky_NLa.png"
+                  species="dog"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VillagerCard from './components/VillagerCard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     VillagerCard
   }
 }
@@ -23,7 +37,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/VillagerCard.vue
+++ src/components/VillagerCard.vue
@@ -1,13 +1,74 @@
 <template>
-  
+  <div id="card" v-if="display">
+    <div id="villager-image"><img :src=img></div>
+    <div id="species">{{ species }}</div>
+    <div id="name">{{ name }}</div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "VillagerCard"
+    name: "VillagerCard",
+    props: {
+      name: String,
+      img: String,
+      species: String
+    },
+    computed: {
+      display: function() {
+        return true;
+      }
+    }
   }
 </script>
 
 <style scoped>
+  #villager-image {
+    height: 200px;
+    display: block;
+    text-align: center;
+    position: relative;
+  }
 
+  #card {
+    display: inline-block;
+    margin: 10px;
+    border-radius: 25px;
+    background: #42ad6b;
+    padding: 20px;
+    width: 200px;
+    /*background: rgba(29,130,101,1);*/
+    background: -moz-linear-gradient(top, rgba(29,130,101,1) 0%, rgba(103,191,147,1) 100%);
+    background: -webkit-gradient(left top, left bottom, color-stop(0%, rgba(29,130,101,1)), color-stop(100%, rgba(103,191,147,1)));
+    background: -webkit-linear-gradient(top, rgba(29,130,101,1) 0%, rgba(103,191,147,1) 100%);
+    background: -o-linear-gradient(top, rgba(29,130,101,1) 0%, rgba(103,191,147,1) 100%);
+    background: -ms-linear-gradient(top, rgba(29,130,101,1) 0%, rgba(103,191,147,1) 100%);
+    background: linear-gradient(to bottom, rgba(29,130,101,1) 0%, rgba(103,191,147,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1d8265', endColorstr='#67bf93', GradientType=0 );
+  }
+
+  #name {
+    color: white;
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  #species {
+    font-variant: small-caps;
+    font-size: 22px;
+    color: white;
+    opacity: 0.5;
+    font-weight: bold;
+  }
+
+  img {
+    max-width:100%;
+    max-height:100%;
+    width: 100px;
+    object-fit: contain;
+    position: absolute;
+    bottom: 0px;
+    margin-left: -50px;
+
+  }
 </style>
